e2c991f1be103d21a4603ad8ea670db950dd5d51
Eduardo-L-R <e.leiva.rocuant@gmail.com>
2020-11-10 21:45:18 -0300

    actualizando

--- Back-end/index.js
+++ Back-end/index.js
@@ -4,36 +4,38 @@ const mongooseDriver = require("mongoose")
 const cors = require("cors");
 const http = require('http');
 const dotenv = require('dotenv');
+const fs = require("fs");
 
 dotenv.config({ path: './.env' });
+dotenv.config({ path: './development.env' });
 const app = express();
 const port = process.env.PORT || 8000;
-app.set('trust proxy', ['::ffff:200.124.47.201']);
 
-app.use(cors())
+app.use((req, res) => {
+    var datetime = new Date();
+    console.log(`Ip: ${req.headers["x-real-ip"] || req.ip.split(':')[req.ip.split(':').length - 1] || req.ips[0] || req.header('x-forwarded-for')} Method: ${req.method} Url: ${req.url} Time: ${datetime}`);
+    fs.appendFile('log.txt', `Ip: '${req.headers["x-real-ip"] || req.ip.split(':')[req.ip.split(':').length - 1] || req.ips[0] || req.header('x-forwarded-for')}' Method: '${req.method}' Url: '${req.url}' Time: '${datetime}',\n`,
+        (err) => { if (err) throw err; console.log('Log actualizado! \n'); });
+});
+
+app.use(cors());
 
 //application/json
 app.use(bodyParser.json());
 
-app.use((req, res) => {
-    console.log(req.header('x-forwarded-for') || req.connection.remoteAddress);
-    console.log(req.ip)
-    console.log(`ip:${req.ips[0] || req.ip.split(':')[req.ip.split(':').length - 1]} method:${req.method} url:${req.url} port:${req.port}`);
-})
-//application/x-www-form-urlencoded
-//app.use(bodyParser.urlencoded()); 
+
 
 //app.use('/links', routesDatabaseLinks);
 
 mongooseDriver.connect('mongodb://localhost:27017/Git_data_2_0?readPreference=primary&appname=MongoDB%20Compass&ssl=false', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log("Conexión con la base de datos establecida");
+    app.listen(port, () => { console.log(`Escuchando sobre el puerto ${port}`); });
     http.get('http://bot.whatismyipaddress.com', (res) => {
         res.setEncoding('utf8');
         res.on('data', (publicIpAdress) => {
-            console.log('you public address is:', `${publicIpAdress}:${port}`);
+            console.log('you public address is:', `${publicIpAdress}:${port} \n`);
         });
     });
-    app.listen(port, () => { console.log(`Escuchando sobre el puerto ${port}`); });
 }).catch(error => {
     console.log(error);
 });
